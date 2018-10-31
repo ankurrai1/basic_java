feb85d70c198c1d0d17f03309218afadda0a4ec1
ankurrai <rai.ankur0007@gmail.com>
2018-10-31 07:07:34 -0700

    added a hello world program

---
+++ helloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+
+    void sayHelloToWorld(){
+        System.out.println("hello World");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld obj = new HelloWorld();
+        obj.sayHelloToWorld();
+    }
+
+}
